97ac50181d4e48183d29545976060b384390f0a5
josedavidbsantos <60598533+josedavidbsantos@users.noreply.github.com>
2020-02-03 22:41:32 -0600

    Update VARIABLEMES.js

--- VARIABLEMES.js
+++ VARIABLEMES.js
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+  
+<script>
+
 var mes = 10
 var nombre = "José David Bolaños Santos"
 var carnet = "Carnet #1210151"
@@ -14,6 +25,12 @@ if (mes==12 || mes==1 || mes==2) {
 } else if (mes==9 || mes==10 || mes==11) {
 	alert("ES OTOÑO");
 
+} else alert("EL MES NO EXISTE");{
+
 }
 
 alert( nombre + carnet);
+
+</script>
+</body>
+</html>
