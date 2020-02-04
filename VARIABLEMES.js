cd5569bd5e86b4479bafe4ed5fc2fa4b69da2109
josedavidbsantos <60598533+josedavidbsantos@users.noreply.github.com>
2020-02-03 21:44:26 -0600

    Add files via upload

---
+++ VARIABLEMES.js
@@ -0,0 +1,19 @@
+var mes = 10
+var nombre = "José David Bolaños Santos"
+var carnet = "Carnet #1210151"
+	
+if (mes==12 || mes==1 || mes==2) {
+	alert("ES INVIERNO");
+	
+} else if (mes==3 || mes==4 || mes==5) {
+	alert("ES PRIMAVERA");
+	
+} else if (mes==6 || mes==7 || mes==8) {
+	alert("ES VERANO");
+	
+} else if (mes==9 || mes==10 || mes==11) {
+	alert("ES OTOÑO");
+
+}
+
+alert( nombre + carnet);
